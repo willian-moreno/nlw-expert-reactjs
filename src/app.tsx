2031d73c7e314899d4506fbed304546fd3f61900
willianam <willianam@uninove.br>
2024-02-08 17:27:03 -0300

    feat: Buscando notas por conteúdo

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import logo from './assets/logo-nlw-expert.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
@@ -10,6 +10,7 @@ interface Note {
 }
 
 export function App() {
+  const [search, setSearch] = useState('')
   const [notes, setNotes] = useState<Note[]>(() => {
     const notesOnStorage = localStorage.getItem('@expert-notes:notes-1.0.0')
 
@@ -19,6 +20,16 @@ export function App() {
 
     return []
   })
+
+  const filteredNotes = filterNotes()
+
+  function filterNotes() {
+    if (!search.length) return notes
+
+    return notes.filter(note => {
+      return note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+    })
+  }
 
   function onNoteCreated(content: string) {
     const newNote: Note = {
@@ -32,6 +43,12 @@ export function App() {
     setNotes(newNotesList)
 
     localStorage.setItem('@expert-notes:notes-1.0.0', JSON.stringify(newNotesList))
+  }
+
+  function handleSearchNote(event: ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value || ''
+
+    setSearch(query)
   }
 
   return (
@@ -48,8 +65,10 @@ export function App() {
               type="text"
               id="query"
               name="query"
+              value={search}
               placeholder="Busque em suas notas..."
               className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 focus:outline-none"
+              onChange={handleSearchNote}
             />
           </form>
 
@@ -61,7 +80,7 @@ export function App() {
         <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 auto-rows-[250px] gap-6 max-w-6xl mx-auto px-5 mb-12">
           <NewNoteCard onNoteCreated={onNoteCreated} />
 
-          {notes.map(note => {
+          {filteredNotes.map(note => {
             return (
               <NoteCard
                 key={note.id}
