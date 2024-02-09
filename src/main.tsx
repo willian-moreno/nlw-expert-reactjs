4cdd4f62385c569c023973de2b93dd13e45f9744
willianam <willianam@uninove.br>
2024-02-09 11:16:07 -0300

    feat: Deploy com as libs gh-pages e react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
 import { Toaster } from 'sonner'
 import { App } from './app'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
     <Toaster richColors duration={5000} />
   </React.StrictMode>,
 )
