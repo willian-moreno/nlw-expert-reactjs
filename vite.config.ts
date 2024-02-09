1b619cc84a999a85ac904d884696061055f3dd0f
willianam <willianam@uninove.br>
2024-02-09 11:44:08 -0300

    feat: Ajuste base url e script de predeploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/nlw-expert-reactjs',
   plugins: [react({
     include: '**/*.tsx',
   })],
